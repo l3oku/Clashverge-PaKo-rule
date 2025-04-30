511b0b022f312151f88f2c264aa57c752918c856
l3oku <121317836+l3oku@users.noreply.github.com>
2025-04-30 20:32:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const yaml = require('js-yaml');
 const app = express();
 
-const FIXED_CONFIG_URL = 'https://raw.githubusercontent.com/6otho/Yaml-PaKo/refs/heads/main/PAKO.yaml';
+const FIXED_CONFIG_URL = 'https://gh.ikuu.eu.org/https://raw.githubusercontent.com/6otho/Yaml-PaKo/refs/heads/main/PAKO.yaml';
 
 async function loadYaml(url) {
   const response = await axios.get(url, { headers: { 'User-Agent': 'Clash Verge' } });
